7e3a940f13fbd8f049e02954b9e42f0781f6c52a
Apt-Rank <aptranking@gmail.com>
2022-04-04 20:03:26 +0900

    스크린샷 테스트

--- js/share.js
+++ js/share.js
@@ -1,16 +1,8 @@
-const capture = async () => {
-  const canvas = document.createElement("canvas");
-  const context = canvas.getContext("2d");
-  const video = document.createElement("video");
-
-  try {
-    const captureStream = await navigator.mediaDevices.getDisplayMedia();
-    video.srcObject = captureStream;
-    context.drawImage(video, 0, 0, window.width, window.height);
-    const frame = canvas.toDataURL("image/png");
-    captureStream.getTracks().forEach(track => track.stop());
-    window.location.href = frame;
-  } catch (err) {
-    console.error("Error: " + err);
+function capture(){
+  if(screen.width < 1024) {
+    document.getElementById("viewport").setAttribute("content", "width=1200px");
   }
-};
+  html2canvas(document.querySelector("body")).then(canvas => {
+    document.body.appendChild(canvas)
+  });
+}
