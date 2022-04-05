04886d27a711472f4cb202951206d91255539908
Apt-Rank <aptranking@gmail.com>
2022-04-05 20:36:57 +0900

    카카오 공유 테스트

--- js/share.js
+++ js/share.js
@@ -1,6 +1,7 @@
 var shareTitle = ""
 var shareText = ""
 var shareURL = ""
+var kakaoKey = "a8a036bfb275fc87317e07f76dccecb2"
 
 function share(shareTitle, shareText, shareURL){
   if (navigator.share) {
@@ -16,4 +17,33 @@ function share(shareTitle, shareText, shareURL){
     // fallback
     shareDialog.classList.add('is-open');
   }
+}
+
+function kakaoShare(shareTitle, shareText, shareURL) {
+  console.log(shareTitle)
+  console.log(shareText)
+  console.log(shareURL)
+  Kakao.Link.sendDefault({    
+    objectType: 'feed',
+    content: {
+      title: shareTitle,
+      description: shareText,
+      imageUrl: '',
+      link: {
+        mobileWebUrl: shareURL,
+        webUrl: shareURL,
+      },
+    },
+    buttons: [
+      {
+        title: '자세히 보기',
+        link: {
+          mobileWebUrl: shareURL,
+          webUrl: shareURL,
+        },
+      },
+    ],
+    // 카카오톡 미설치 시 카카오톡 설치 경로이동
+    installTalk: true,
+  })
 }
